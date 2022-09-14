db12bd7c477dadfe054291d5ce468351a1ee8e7c
roderick <hidahza@gmail.com>
2022-09-14 10:42:30 -0400

    test mac commit

---
+++ HelloWorld/src/HellowWorld/HelloWorld.py
@@ -0,0 +1,20 @@
+'''
+Created on Sep 14, 2022
+
+@author: roderickf
+'''
+
+
+
+minPower = 6
+maxPower = 7
+
+
+def returnSomething():
+    strStatement = "between " + str({minPower}) + " and " + str({maxPower})
+    return strStatement
+
+
+if __name__ == "__main__":
+    
+    print(returnSomething())
